843e8aeca9d1ee00e39e27c83dcdb40252416a09
Bernhard Kralofsky <bernhard@kralofsky.com>
2021-01-15 13:00:18 +0100

    add function to install requirements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 import threading
 from time import time, sleep
 
@@ -15,34 +16,46 @@ def index():
     return 'Hello World'
 
 
+def process_response(process, timeout=60):
+    stoptime = [time() + timeout]
+
+    def stop():
+        while True:
+            if time() > stoptime[0]:
+                process.kill()
+                break
+            sleep(stoptime[0] - time())
+
+    threading.Thread(target=stop).start()
+
+    def generate():
+        while True:
+            stoptime[0] = time() + timeout
+            r = process.stdout.readline()
+            if len(r) == 0:
+                break
+            yield r
+
+    return Response(generate(), mimetype='text/plain', content_type='text/event-stream')
+
+
+@app.route('/install')
+def install():
+    p = subprocess.Popen([sys.executable, '-m', 'pip', 'install', '-r', SCRIPT_DIR + '/requirements.txt'],
+                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return process_response(p, 600)
+
+
 @app.route('/run/<script>')
 def run(script):
     scripts = os.listdir(SCRIPT_DIR)
     if script + '.py' not in scripts:
         abort(404, 'Script not found')
 
-    p = subprocess.Popen(['python', SCRIPT_DIR + '/' + script + '.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(['python', SCRIPT_DIR + '/' + script + '.py'],
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-    timeout = [time() + 60]
-
-    def stop():
-        while True:
-            if time() > timeout[0]:
-                p.kill()
-                break
-            sleep(timeout[0] - time())
-
-    threading.Thread(target=stop).start()
-
-    def generate():
-        while True:
-            timeout[0] = time() + 60
-            r = p.stdout.readline()
-            if len(r) == 0:
-                break
-            yield r
-
-    return Response(generate(), mimetype='text/plain', content_type='text/event-stream')
+    return process_response(p)
 
 
 if __name__ == '__main__':
