0de5d5c518f805605a31a2f5e4c990c74573f7e4
Bernhard Kralofsky <bernhard@kralofsky.com>
2021-01-15 12:34:39 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import threading
+from time import time, sleep
+
+from flask import Flask, Response, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+@app.route('/run/<script>')
+def run(script):
+    scripts = os.listdir("scripts")
+    if script + ".py" not in scripts:
+        abort(404, "Script not found")
+
+    p = subprocess.Popen(["python", "scripts/" + script + ".py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    timeout = [time() + 60]
+
+    def stop():
+        while True:
+            if time() > timeout[0]:
+                p.kill()
+                break
+            sleep(timeout[0] - time())
+
+    threading.Thread(target=stop).start()
+
+    def generate():
+        while True:
+            timeout[0] = time() + 60
+            r = p.stdout.readline()
+            if len(r) == 0:
+                break
+            yield r
+
+    return Response(generate(), mimetype='text/plain', content_type='text/event-stream')
+
+
+if __name__ == '__main__':
+    app.run()
